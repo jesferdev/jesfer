58588bbd22cb3c88d09870200dc5b6ca8b59c69b
Jesús Fernando <jesusfer_nan2@hotmail.com>
2023-04-08 22:58:31 -0500

    page home (no responsive)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "moving-background": "moving-background 5s ease-in-out",
+      },
+      keyframes: {
+        "moving-background": {
+          "0%": {
+            transform: "translateY(0)",
+            opacity: 0,
+          },
+          "66%": {
+            opacity: 0.4,
+          },
+          "100%": {
+            transform: "translateY(-150px)",
+            opacity: 0,
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
